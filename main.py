64461437e4755f1bc9cfa38204016cc9e2bbd9bc
ChrisChrisRivera <60199028+ChrisChrisRivera@users.noreply.github.com>
2020-01-29 15:26:18 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+x= 4
+if x ==5:
+  print("hi")
+else:
+  print("yes")
+
+print(1==1 and 2==2)
+print(34==35 and 55==55)
+
+i = 2
+while i <=36:
+  print(i)
+  i = i + 2
+print("Finished!")
+
+i = 5
+while i <=40:
+  print(i)
+  i = i + 5
+print("done!")
+
+nums = [5, 6, 4]
+nums[2]= 34
+print(nums)
+
+print ("Hello World!")
+print ("Hello Again")
+print ("I like typing this.")
+print ("This is fun.")
+print ('yay! Printing.')
+print ("I'd much rather you'not'.")
+print ('I "said" do not touch this.')
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.print
+
+print ("I could have a code like this.")# and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print("This code will run.")
+# It prints the statement
+print("I will now count my chickens:")
+# Calculates the amount of Hens
+print("Hens", 25+30/6)
+# Calculates the amount of Roosters
+print("Roosters", 100-25*3%4)
+# States that they will count eggs
+print("Now I will count the eggs:")
+# Performs a calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1/4 + 6)
+# True or False statement
+print("Is it true that 3 + 2 < 5 - 7?")
+# True or False statement
+print(3+2<5-7)
+# Asking the Answer for 3+2
+print("What us 3+2?", 3+2)
+# Asking the Answer for 5-7
+print("What is 5-7?", 5-7)
+# A clear statement
+print("Oh, that's why it's False.")
+# Asking for more statements
+print("How about some more.")
+# Asking if greater than 5
+print("Is it greater?", 5>-2)
+# Asking if greater than
+print("Is it greater or equal?", 5>=-2)
+# Asking if less than
+print("Is it less or equal?", 5<=-2)
