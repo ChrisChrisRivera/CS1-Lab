75d37ecedc9a3ce2600f98dcb802d52edc72788d
ChrisChrisRivera <60199028+ChrisChrisRivera@users.noreply.github.com>
2020-02-19 00:07:39 -0600

    Add files via upload

---
+++ lvl 1.py
@@ -0,0 +1,22 @@
+stateName= "Computer"
+for letter in stateName:
+  print(letter)
+
+for i in range (1,10):
+  print(i)
+
+
+print(int(input("enter number for list: ")))
+xValue= 20
+while xValue>=10:
+  print(xValue)
+  xValue= xValue - .5
+else:
+  print("Try for another list")
+
+print(int(input("one more please: ")))
+dValue= 5
+while dValue>=1:
+  print(dValue)
+  dValue= dValue - .5
+  
