23993a458e1106880f6979172a8a9ad1be460132
ChrisChrisRivera <60199028+ChrisChrisRivera@users.noreply.github.com>
2020-02-18 22:57:03 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-#There are 100 cars
-cars= 100
-#4 seats in the cars
-space_in_a_car= 4.0
-#there are 30 drivers
-drivers= 30
-#there are 90 passengers
-passengers= 90
-#cars not used
-cars_not_driven= cars - drivers
-#cars that are being driven 
-cars_driven= drivers
-#the amount of people that can carpool
-carpool_capacity= cars_driven * space_in_a_car
-#average amount of people per car
-average_passengers_per_car= passengers//cars_driven
+xValue= 20
+while xValue>=10:
+  print(xValue)
+  xValue= xValue - .5
 
+for i in range(25):
+    import math
+    print(math.sqrt(i))
 
-print("There are", cars, "cars available.")
-print("There are only", drivers, "drivers available.")
-print("There will be", cars_not_driven, "empty cars today.")
-print("We can transport", carpool_capacity, "people today.")
-print("We have", passengers, "to carpool today.")
-print("We need to put about", average_passengers_per_car, "in each car.")
-#There was an error because there is an underscore after "car" in "carpool"
-#1. It doesnt matter how you have "4" as long as you have it, the way you can change that is by add another dashed line while dividing like so "//"
-#2. it can be placed anywhere relative to the significant digits of the number
-j=13
-i=26
-print(j*i)
+vaild= False
+while not vaild:
+  value= int(input("number please"))
+  if value > 0 and value < 100:
+    valid= True
+  else:
+    print("CORRECT!")
